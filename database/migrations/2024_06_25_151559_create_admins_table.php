e5d22838b4d2fc5594a6cf1bdaab6784130a73ed
Nazmul66 <hnazmul748@gmail.com>
2024-06-26 00:05:31 +0600

    AdminTableSeeder create and upload data into admin tables

--- database/migrations/2024_06_25_151559_create_admins_table.php
+++ database/migrations/2024_06_25_151559_create_admins_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string("phone");
             $table->string("email")->unique();
             $table->string("password");
-            $table->string("image");
+            $table->string("image")->nullable();
             $table->tinyInteger("status");
             $table->timestamps();
         });
