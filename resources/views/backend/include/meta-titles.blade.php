ccfc137c9774175785d38533a38de4fe3a315731
Nazmul66 <hnazmul748@gmail.com>
2024-06-25 21:12:02 +0600

    admin panel mastering templates

---
+++ resources/views/backend/include/meta-titles.blade.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>
+        @stack('meta-title')
+    </title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('/public/assets/images/favicon.ico') }}">
+
+    <!-- plugin css -->
+    <link href="{{ asset('/public/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- preloader css -->
+    <link rel="stylesheet" href="{{ asset('/public/assets/css/preloader.min.css') }}" type="text/css" />
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('/public/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('/public/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('/public/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    
+    @stack('style-css')
+</head>
